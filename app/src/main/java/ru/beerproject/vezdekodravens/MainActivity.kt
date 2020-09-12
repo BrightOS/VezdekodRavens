80fa8b558a2755da0c99dfd1309fd29771fe0e56
BrightOS <dbarov3@gmail.com>
2020-09-12 14:18:53 +0500

    кто прочитал, тот собака

--- app/src/main/java/ru/beerproject/vezdekodravens/MainActivity.kt
+++ app/src/main/java/ru/beerproject/vezdekodravens/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.transition.MaterialSharedAxis
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.type_select_activity.*
 
